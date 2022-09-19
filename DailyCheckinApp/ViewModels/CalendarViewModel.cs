7c7725de7c1b61b8644da62e79bf2a218dcf065d
sfoslund <sfoslund@microsoft.com>
2022-09-19 14:47:05 -0700

    Make calendar selectable

--- DailyCheckinApp/ViewModels/CalendarViewModel.cs
+++ DailyCheckinApp/ViewModels/CalendarViewModel.cs
@@ -1,11 +1,42 @@
-﻿
-
+﻿using System.Windows.Input;
 using XCalendar.Core.Models;
 
 namespace DailyCheckinApp.ViewModels
 {
     internal class CalendarViewModel
     {
-        public Calendar<CalendarDay> Calendar { get; set; } = new Calendar<CalendarDay>();
+        #region Properties
+        public Calendar<CalendarDay> Calendar { get; set; } = new Calendar<CalendarDay>()
+        {
+            SelectionType = XCalendar.Core.Enums.SelectionType.Single,
+            SelectionAction = XCalendar.Core.Enums.SelectionAction.Replace,
+            SelectedDates = new ObservableRangeCollection<DateTime>() { DateTime.Today },
+        };
+        #endregion
+
+        #region Commands
+        public ICommand NavigateCalendarCommand { get; set; }
+        public ICommand ChangeDateSelectionCommand { get; set; }
+        #endregion
+
+        #region Constructors
+        public CalendarViewModel()
+        {
+            NavigateCalendarCommand = new Command<int>(NavigateCalendar);
+            ChangeDateSelectionCommand = new Command<DateTime>(ChangeDateSelection);
+        }
+        #endregion
+
+        #region Methods
+        public void NavigateCalendar(int Amount)
+        {
+            Calendar?.NavigateCalendar(Amount);
+        }
+
+        public void ChangeDateSelection(DateTime DateTime)
+        {
+            Calendar?.ChangeDateSelection(DateTime);
+        }
+        #endregion
     }
 }
